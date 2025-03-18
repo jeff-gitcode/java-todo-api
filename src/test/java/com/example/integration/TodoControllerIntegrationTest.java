1b0bf5a6c0d6ed4f4a81952139ae73b118f30fb6
jeff-gitcode <jeff.gitcode@gmail.com>
2025-03-18 17:42:35 +1100

    integration test

---
+++ src/test/java/com/example/integration/TodoControllerIntegrationTest.java
@@ -0,0 +1,117 @@
+package com.example.integration;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.example.application.dto.TodoDTO;
+import com.example.application.interfaces.TodoRepository;
+import com.example.domain.model.Todo;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class TodoControllerIntegrationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private TodoRepository todoRepository;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @BeforeEach
+    public void setUp() {
+        todoRepository.deleteAll();
+    }
+
+    @Test
+    public void testGetAllTodos() throws Exception {
+        Todo todo1 = new Todo();
+        todo1.setTitle("Todo 1");
+        todoRepository.save(todo1);
+
+        Todo todo2 = new Todo();
+        todo2.setTitle("Todo 2");
+        todoRepository.save(todo2);
+
+        mockMvc.perform(get("/todos"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].title").value("Todo 1"))
+                .andExpect(jsonPath("$[1].title").value("Todo 2"));
+    }
+
+    @Test
+    public void testGetTodoById() throws Exception {
+        Todo todo = new Todo();
+        todo.setTitle("Todo 1");
+        todo = todoRepository.save(todo);
+
+        mockMvc.perform(get("/todos/" + todo.getId()))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.title").value("Todo 1"));
+    }
+
+    @Test
+    public void testCreateTodo() throws Exception {
+        TodoDTO todoDTO = new TodoDTO();
+        todoDTO.setTitle("New Todo");
+
+        mockMvc.perform(post("/todos")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(todoDTO)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.title").value("New Todo"));
+    }
+
+    @Test
+    public void testUpdateTodo() throws Exception {
+        Todo todo = new Todo();
+        todo.setTitle("Old Todo");
+        todo = todoRepository.save(todo);
+
+        TodoDTO todoDTO = new TodoDTO();
+        todoDTO.setTitle("Updated Todo");
+
+        mockMvc.perform(put("/todos/" + todo.getId())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(todoDTO)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.title").value("Updated Todo"));
+    }
+
+    @Test
+    public void testDeleteTodo() throws Exception {
+        Todo todo = new Todo();
+        todo.setTitle("Todo to be deleted");
+        todo = todoRepository.save(todo);
+
+        mockMvc.perform(delete("/todos/" + todo.getId()))
+                .andExpect(status().isOk());
+
+                    // Verify that the todo has been deleted
+        boolean exists = todoRepository.existsById(todo.getId());
+        assertFalse(exists, "Todo should be deleted");
+
+        // Verify that the todo is not found, but it is optional
+        mockMvc.perform(get("/todos/" + todo.getId()))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.title").doesNotExist());
+    }
+}
